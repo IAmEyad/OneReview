fc40b41e8750a7641dbdd03f9cd8c600d99f8af5
ForgeStudios <mkeays@gmu.edu>
2018-09-23 11:32:19 -0400

    COMMIT

--- OneReview/web/views.py
+++ OneReview/web/views.py
@@ -26,5 +26,6 @@ def verify(request):
     return HttpResponseRedirect('https://api-sandbox.capitalone.com/oauth2/authorize?client_id=8866cf47fe3a4522850c976d63264010&grant_type=authorization_code&redirect_uri=http://127.0.0.1:8000/&scope=verify&response_type=code')
 
 def get_query(request):
+    print("get_query")
     form = QueryForm(request.POST)
     return render(request, 'results.html', {'form' : form})
