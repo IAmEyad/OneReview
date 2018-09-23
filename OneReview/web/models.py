3a5e842b4c67403baf39a11cd794ed82d333162d
Jay Wilson <jwilso48@gmu.edu>
2018-09-23 08:32:44 -0400

    stash boyes

--- OneReview/web/models.py
+++ OneReview/web/models.py
@@ -6,12 +6,11 @@ from os import path
 
 # Create your models here.
 class User(models.Model):
-<<<<<<< HEAD
     full_name = models.CharField(max_length=70)
 
 class Cloud(models.Model):
     # get data directory (using getcwd() is needed to support running example in generated IPython notebook)
-    def generate_cloud(cloud_path):
+    def generate_cloud(cloud_text):
         # get path for wordcloud
         # d = path.dirname(__file__) if "__file__" in locals() else os.getcwd()
 
@@ -23,9 +22,3 @@ class Cloud(models.Model):
 
         image = wordcloud.to_image()
         image.show()
-=======
-    client_id = "8866cf47fe3a4522850c976d63264010"
-    client_secret = "c97562a079ce4b855e372e8f8d9d6976"
-    redirect_uri = "https://127.0.0.1"
-    access_token ="eyJlbmMiOiJBMTI4Q0JDLUhTMjU2IiwicGNrIjoxLCJhbGciOiJkaXIiLCJ0diI6Miwia2lkIjoicjRxIn0..xaLM_sX8uPsqyx4puK9YDg.QhewccHzdiAeDfAU1y7Sn6v_5ShXxMQcqvZcui72lz7Ih2Dv6zid1HdkjWkONbTPyOWKOs41x8DwdMPfocTFy2_MmX0VkfAwjqQoih4MVfky-uchuV7TNDMjxp9bRY6-JgGsmUDXObBBfh1nCIWauVvSP7Tb5-sa14QWrt5-TYd4xxOFq9FBtSa1ivJvGsUQDIsP3_9_K_lKFsMJyCVDmGiuPlCKdDliMkrFKISdxiVJz4Sj1q8eIG2shzs2GSQUHBICJDmdxydmWL1EsELo-odPmik78Xi7XyLVpglaYYQ.acTU2RTA8zKCU985gJyI4g"
->>>>>>> Django-Backend
