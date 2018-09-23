9cb67402db85f96c4527dad6569f2ff80f2ec253
Eyad Hasan <ehasan3@gmu.edu>
2018-09-23 01:55:54 -0400

    Working on getting the backend working correctly

--- OneReview/web/models.py
+++ OneReview/web/models.py
@@ -2,4 +2,7 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    full_name = models.CharField(max_length=70)
+    client_id = "8866cf47fe3a4522850c976d63264010"
+    client_secret = "c97562a079ce4b855e372e8f8d9d6976"
+    redirect_uri = "https://127.0.0.1"
+    access_token ="eyJlbmMiOiJBMTI4Q0JDLUhTMjU2IiwicGNrIjoxLCJhbGciOiJkaXIiLCJ0diI6Miwia2lkIjoicjRxIn0..xaLM_sX8uPsqyx4puK9YDg.QhewccHzdiAeDfAU1y7Sn6v_5ShXxMQcqvZcui72lz7Ih2Dv6zid1HdkjWkONbTPyOWKOs41x8DwdMPfocTFy2_MmX0VkfAwjqQoih4MVfky-uchuV7TNDMjxp9bRY6-JgGsmUDXObBBfh1nCIWauVvSP7Tb5-sa14QWrt5-TYd4xxOFq9FBtSa1ivJvGsUQDIsP3_9_K_lKFsMJyCVDmGiuPlCKdDliMkrFKISdxiVJz4Sj1q8eIG2shzs2GSQUHBICJDmdxydmWL1EsELo-odPmik78Xi7XyLVpglaYYQ.acTU2RTA8zKCU985gJyI4g"
--- OneReview/web/views.py
+++ OneReview/web/views.py
@@ -1,4 +1,13 @@
+import requests
 from django.http import HttpResponse
+from django.shortcuts import render
+from . import models
 
 def index(request):
-    return HttpResponse("Hello, world.  You're at the polls index.")
+    return render(request, 'index.html')
+
+def verify(request):
+    if request.method == 'POST':
+        url = 'https://api-sandbox.capitalone.com/oauth2/token'
+        r = requests.post(url, data =[('client_id',models.User.client_id), 'client_secret' = [models.User.client_secret], 'grant_type' = ['client_credentials']})
+    return render(request, 'login.html')
